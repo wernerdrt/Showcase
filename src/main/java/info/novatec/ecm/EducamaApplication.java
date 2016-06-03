a707644c6941f6c5a5a025a7db45306c2afc17f2
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2016-06-03 10:43:37 +0200

    Add initial spring boot project based on web.

---
+++ src/main/java/info/novatec/ecm/EducamaApplication.java
@@ -0,0 +1,12 @@
+package info.novatec.ecm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducamaApplication.class, args);
+	}
+}
