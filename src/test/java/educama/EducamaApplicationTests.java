1606bb8be898a0109e46af40f8408214754a8a59
Eberhard Heber <eberhard.heber@novatec-gmbh.de>
2016-09-19 10:11:14 +0200

    Added Javadoc to EducamaApplicationTests

--- src/test/java/educama/EducamaApplicationTests.java
+++ src/test/java/educama/EducamaApplicationTests.java
@@ -7,12 +7,20 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+/**
+ * This test is the integration test for the spring boot implementation.
+ * 
+ *
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = EducamaApplication.class)
 @WebAppConfiguration
 @IntegrationTest
 public class EducamaApplicationTests {
 
+	/**
+	 * this test actually tests that the Spring Context is created successfully, i.e. the application starts successfully.
+	 */
 	@Test
 	public void contextLoads() {
 	}
