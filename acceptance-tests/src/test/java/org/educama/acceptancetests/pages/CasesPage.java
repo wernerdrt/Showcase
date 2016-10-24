683ee5f5a46d55cad134f3ebed332180333a56a5
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2016-10-24 20:19:33 +0200

    ECM-35: Integration tests are now run as part of the build, i.e. not after the deployment, so that errors are found earlier, e.g. during pull requests.

--- acceptance-tests/src/test/java/org/educama/acceptancetests/pages/CasesPage.java
+++ acceptance-tests/src/test/java/org/educama/acceptancetests/pages/CasesPage.java
@@ -10,7 +10,7 @@ import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl("http://localhost:8081")
+@DefaultUrl("http://localhost:8090")
 public class CasesPage extends PageObject {
 
 	@FindBy(id = "casePage")
