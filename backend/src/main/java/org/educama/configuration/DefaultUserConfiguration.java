8ea93f1411b228ce9bb91278b4b98aefcbb91a9c
Thorsten Jakoby <tj@novatec-gmbh.de>
2016-11-16 19:47:40 +0100

    ECM-45: create default user if none exists

---
+++ backend/src/main/java/org/educama/configuration/DefaultUserConfiguration.java
@@ -0,0 +1,95 @@
+package org.educama.configuration;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import org.camunda.bpm.engine.authorization.Authorization;
+import org.camunda.bpm.engine.authorization.Resources;
+import org.camunda.bpm.engine.AuthorizationService;
+import org.camunda.bpm.engine.IdentityService;
+import org.camunda.bpm.engine.identity.Group;
+import org.camunda.bpm.engine.task.TaskQuery;
+import org.camunda.bpm.engine.FilterService;
+import org.camunda.bpm.engine.filter.Filter;
+import org.camunda.bpm.engine.identity.User;
+import org.camunda.bpm.engine.TaskService;
+
+@Component
+public class DefaultUserConfiguration {
+
+    @Autowired
+    IdentityService identityService;
+
+    @Autowired
+    AuthorizationService authorizationService;
+
+    @Autowired
+    TaskService taskService;
+
+    @Autowired
+    FilterService filterService;
+
+
+    @Value("${org.educama.configuration.adminUser}")
+    private String adminUsername;
+
+    @Value("${org.educama.configuration.adminPassword}")
+    private String adminPassword;
+
+
+    @PostConstruct
+    protected void init() {
+        if(userExists(adminUsername))
+            return;
+
+        User user = createDefaultUser(adminUsername, adminPassword);
+        Group adminGroup = createAdminGroup(user);
+        grantAuthorizationWithPermissions(adminGroup);
+        createAssignedTaskQuery();
+    }
+
+    private boolean userExists(String username) {
+        return (identityService.createUserQuery().userId(username).count() > 0);
+    }
+
+    private User createDefaultUser(String username, String password) {
+        User user = identityService.newUser(username);
+        user.setPassword(password);
+        user.setFirstName("EduCaMa");
+        user.setLastName("User");
+        identityService.saveUser(user);
+
+        return user;
+    }
+
+    private Group createAdminGroup(User user) {
+        Group adminGroup = identityService.newGroup("camunda-admin");
+        identityService.saveGroup(adminGroup);
+        identityService.createMembership(user.getId(), adminGroup.getId());
+
+        return adminGroup;
+    }
+
+    private void grantAuthorizationWithPermissions(Group adminGroup) {
+        Authorization authorization = authorizationService.createNewAuthorization(Authorization.AUTH_TYPE_GRANT);
+        authorization.setGroupId(adminGroup.getId());
+        authorization.setResource(Resources.USER);
+        authorization.addPermission(org.camunda.bpm.engine.authorization.Permissions.ALL);
+        authorizationService.saveAuthorization(authorization);
+    }
+
+    /**
+     * creates a task query to select task to the logged in user.
+     * Otherwise you would have to manually create a filter in TaskList after each boot up
+     */
+    private void createAssignedTaskQuery() {
+        TaskQuery query = taskService.createTaskQuery().taskAssignee(adminUsername);
+        Filter taskFilter = filterService.newTaskFilter("assigned Tasks");
+        taskFilter.setOwner(adminUsername);
+        taskFilter.setQuery(query);
+        filterService.saveFilter(taskFilter);
+    }
+}
