1ef2e8c1bce78812b38ffbc7816fc8f38d197889
Tobias Schaefer <tobias.schaefer@novatec-gmbh.de>
2016-12-16 13:58:08 +0100

    ECM-63: Update to Camunda Spring Boot Starter 2.0.0 and thereby update to Camunda 7.6 (and Spring Boot 1.4.2).

--- backend/src/test/java/org/educama/EducamaApplicationTests.java
+++ backend/src/test/java/org/educama/EducamaApplicationTests.java
@@ -1,26 +1,18 @@
 package org.educama;
 
-import org.educama.EducamaApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
 
-/**
- * This test is the integration test for the spring boot implementation.
- * 
- *
- */
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = EducamaApplication.class)
-@WebAppConfiguration
-@IntegrationTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 public class EducamaApplicationTests {
 
 	/**
-	 * this test actually tests that the Spring Context is created successfully, i.e. the application starts successfully.
+	 * This test verifies that the Spring Context is created successfully, i.e.
+	 * the application starts successfully.
 	 */
 	@Test
 	public void contextLoads() {
