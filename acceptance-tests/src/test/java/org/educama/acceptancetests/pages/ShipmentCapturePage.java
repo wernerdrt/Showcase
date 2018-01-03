d0ffd76d662d71af7d695b5f73b3ff8fb5ac2638
Martin Schmollinger <martin.schmollinger@gmail.com>
2018-01-03 22:28:41 +0100

    ECM-161: Repaired automation of acceptance tests

--- acceptance-tests/src/test/java/org/educama/acceptancetests/pages/ShipmentCapturePage.java
+++ acceptance-tests/src/test/java/org/educama/acceptancetests/pages/ShipmentCapturePage.java
@@ -3,6 +3,8 @@ package org.educama.acceptancetests.pages;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
+
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
 /**
@@ -11,14 +13,11 @@ import org.openqa.selenium.WebElement;
 @DefaultUrl("http://localhost:8090/shipments/capture")
 public class ShipmentCapturePage extends PageObject {
 
-    @FindBy(id = "shipment-capture-component_customer")
-    WebElement formFieldCustomer;
+    @FindBy(id = "shipment-capture-component_sender")
+    WebElement formFieldSender;
 
-    @FindBy(id = "shipment-capture-component_sender-address")
-    WebElement formFieldSenderAddress;
-
-    @FindBy(id = "shipment-capture-component_receiver-address")
-    WebElement formFieldReceiverAddress;
+    @FindBy(id = "shipment-capture-component_receiver")
+    WebElement formFieldReceiver;
 
     @FindBy(id = "shipment-capture-component_cancel-button")
     WebElement cancelButton;
@@ -27,9 +26,15 @@ public class ShipmentCapturePage extends PageObject {
     WebElement createButton;
 
     public void createShipment() {
-        formFieldCustomer.sendKeys("John Doe");
-        formFieldSenderAddress.sendKeys("Dummy Street, 12345 Dummy Sender Town");
-        formFieldReceiverAddress.sendKeys("Dummy Street, 12345 Dummy Receiver Town");
+        formFieldSender.sendKeys("Apple");
+        $("//ul//li[contains(.,\"Apple\")]").waitUntilVisible();
+        formFieldSender.sendKeys(Keys.DOWN);
+        formFieldSender.sendKeys(Keys.RETURN);
+
+        formFieldReceiver.sendKeys("NovaTec Consulting GmbH");
+        $("//ul//li[contains(.,\"NovaTec Consulting GmbH\")]").waitUntilVisible();
+        formFieldReceiver.sendKeys(Keys.DOWN);
+        formFieldReceiver.sendKeys(Keys.RETURN);
         clickOn(createButton);
     }
 }
